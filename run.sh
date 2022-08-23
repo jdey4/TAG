9ed0112356d829c819ebddaa7f58099d40c01123
jdey4 <jdey4@jhmi.edu>
2022-08-23 13:27:41 -0400

    code fix

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ do
     for shift in 1 2 3 4 5 6;
     do
         echo 'Doing shift ' $shift;
-        python3 -m main --dataset cifar100 --tasks 2 --epochs-per-task 20 --lr .03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'agem' --mem-size 1 --angle $angle --shift $shift
+        python3 -m main --dataset cifar100 --tasks 2 --epochs-per-task 10 --lr .03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'agem' --mem-size 1 --angle $angle --shift $shift
     done
 done
 ################################
@@ -16,7 +16,7 @@ do
     for shift in 1 2 3 4 5 6;
     do
         echo 'Doing shift ' $shift;
-        python3 -m main --dataset cifar100 --tasks 2 --epochs-per-task 20 --lr 0.00025 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'tag' --b 5 --tag-opt 'rms' --angle $angle --shift $shift
+        python3 -m main --dataset cifar100 --tasks 2 --epochs-per-task 10 --lr 0.00025 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'tag' --b 5 --tag-opt 'rms' --angle $angle --shift $shift
     done
 done
 ###############################
@@ -26,7 +26,7 @@ do
     for shift in 1 2 3 4 5 6;
     do
         echo 'Doing shift ' $shift;
-        python3 -m main --dataset cifar100 --tasks 2 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --angle $angle --shift $shift
+        python3 -m main --dataset cifar100 --tasks 2 --epochs-per-task 10 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --angle $angle --shift $shift
     done
 done
 ###########################
