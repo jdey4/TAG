ddee5ab4c920be427a3911c621f16ddd4b6e692c
jdey4 <jdey4@jhmi.edu>
2022-08-24 11:16:06 -0400

    new res

---
+++ run_single.sh
@@ -0,0 +1,7 @@
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.00025 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'tag' --b 5 --tag-opt 'rms' --slot 0 --shift 1 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.00025 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'tag' --b 5 --tag-opt 'rms' --slot 0 --shift 2 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.00025 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'tag' --b 5 --tag-opt 'rms' --slot 0 --shift 3 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.00025 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'tag' --b 5 --tag-opt 'rms' --slot 0 --shift 4 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.00025 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'tag' --b 5 --tag-opt 'rms' --slot 0 --shift 5 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.00025 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'tag' --b 5 --tag-opt 'rms' --slot 0 --shift 6 --single_task 1
+
