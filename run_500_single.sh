db12eb2079747f57469ffeb80a26ca252561f871
jdey4 <jdey4@jhmi.edu>
2022-08-29 22:44:45 -0400

    converted file

---
+++ run_500_single.sh
@@ -0,0 +1,69 @@
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 0 --shift 1 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 0 --shift 2 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 0 --shift 3 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 0 --shift 4 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 0 --shift 5 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 0 --shift 6 --single_task 1
+
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 1 --shift 1 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 1 --shift 2 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 1 --shift 3 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 1 --shift 4 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 1 --shift 5 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 1 --shift 6 --single_task 1
+
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 2 --shift 1 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 2 --shift 2 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 2 --shift 3 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 2 --shift 4 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 2 --shift 5 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 2 --shift 6 --single_task 1
+
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 3 --shift 1 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 3 --shift 2 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 3 --shift 3 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 3 --shift 4 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 3 --shift 5 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 3 --shift 6 --single_task 1
+
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 4 --shift 1 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 4 --shift 2 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 4 --shift 3 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 4 --shift 4 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 4 --shift 5 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 4 --shift 6 --single_task 1
+
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 5 --shift 1 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 5 --shift 2 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 5 --shift 3 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 5 --shift 4 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 5 --shift 5 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 5 --shift 6 --single_task 1
+
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 6 --shift 1 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 6 --shift 2 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 6 --shift 3 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 6 --shift 4 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 6 --shift 5 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 6 --shift 6 --single_task 1
+
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 7 --shift 1 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 7 --shift 2 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 7 --shift 3 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 7 --shift 4 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 7 --shift 5 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 7 --shift 6 --single_task 1
+
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 8 --shift 1 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 8 --shift 2 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 8 --shift 3 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 8 --shift 4 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 8 --shift 5 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 8 --shift 6 --single_task 1
+
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 9 --shift 1 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 9 --shift 2 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 9 --shift 3 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 9 --shift 4 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 9 --shift 5 --single_task 1
+python3 -m main --dataset cifar100 --tasks 10 --epochs-per-task 20 --lr 0.03 --gamma 1.0 --batch-size 10 --dropout 0.0 --runs 1 --opt 'er' --mem-size 1 --slot 9 --shift 6 --single_task 1
