26eea6e5a7a53c3d2048acb7571606e9d20e95b8
jdey4 <jdey4@jhmi.edu>
2022-09-01 22:58:41 -0400

    converted files

---
+++ file_conversion.py
@@ -0,0 +1,53 @@
+#%%
+import pickle
+import numpy as np
+import pandas as pd
+# %%
+with open('/Users/jayantadey/TAG/results/five_dataset/agem.pickle', 'rb') as f:
+    res = pickle.load(f)
+
+# %%
+total_tasks = 5
+algo = 'agem'
+# %%
+df = pd.DataFrame()
+df_single_task = pd.DataFrame()
+shifts = []
+tasks = []
+base_tasks = []
+accuracies_across_tasks = []
+shifts_single = []
+tasks_single = []
+accuracy_single = []
+
+multitask_file = 'results/five_dataset/'+algo+'.pickle'
+singletask_file = 'results/five_dataset/'+algo+'_'+'single_task.pickle'
+
+with open(multitask_file, 'rb') as f:
+    res_multi = pickle.load(f)
+
+with open(singletask_file, 'rb') as f:
+    res_single = pickle.load(f)
+
+
+for ii in range(5):
+    tasks_single.append(ii+1)
+    accuracy_single.append(res_single[ii]/100)
+    for jj in range(ii+1):
+        tasks.append(jj+1)
+        base_tasks.append(ii+1)
+        accuracies_across_tasks.append(res_multi[jj+1][ii]/100)
+
+df['task'] = tasks
+df['base_task'] = base_tasks
+df['accuracy'] = accuracies_across_tasks
+
+df_single_task = pd.DataFrame()
+df_single_task['task'] = tasks_single
+df_single_task['accuracy'] = accuracy_single
+
+summary = (df,df_single_task)
+file_to_save = 'converted_files/five_dataset/'+algo+'.pickle'
+with open(file_to_save, 'wb') as f:
+    pickle.dump(summary, f)
+# %%
